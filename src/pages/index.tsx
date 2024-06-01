f6b05a85f3748185f04442933434562d6ac53535
svenac20 <sven.scekic@gmail.com>
2024-06-01 19:10:37 +0200

    add background image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/router'
 import { useState } from 'react'
 import useGameSettingsStore from '../store/game-settings-store'
 import usePlayerSquadStore from '../store/player-squad-store'
+import Image from 'next/image'
+import background from '../public/images/background-entry-screen.png'
 
 const Home: NextPage = () => {
   const [numberOfPlayers, setNumberOfPlayers] = useState(0)
@@ -18,9 +20,10 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="align-center flex h-full w-full justify-center p-4">
-      <div className="flex  flex-col justify-center gap-5">
-        <div>
+    <div className="align-center flex h-full w-full justify-center p-4 relative">
+      <Image src={background} alt={"background"} fill={true} className='-z-10'></Image>
+      <div className="flex  flex-col justify-center gap-14 absolute top-[30%] w-[20%]">
+        <div className="w-full">
           <label htmlFor="players" className="block font-bold">
             Enter number of players:
           </label>
@@ -28,7 +31,7 @@ const Home: NextPage = () => {
             type="number"
             id="players"
             name="players"
-            className="mt-0 h-10 rounded pl-2 text-black"
+            className="mt-0 h-10 rounded pl-2 text-black w-full"
             onChange={(e) => setNumberOfPlayers(Number(e.target.value))}
           ></input>
         </div>
@@ -40,12 +43,12 @@ const Home: NextPage = () => {
             type="number"
             id="budget"
             name="budget"
-            className="mt-0 h-10 rounded pl-2 text-black"
+            className="mt-0 h-10 rounded pl-2 text-black w-full"
             onChange={(e) => setBudget(Number(e.target.value))}
           ></input>
         </div>
         <button
-          className="bg-black-100 mt-2 h-10 rounded border-2 font-bold"
+          className="bg-black-100 mt-6 h-10 rounded border-2 font-bold"
           onClick={startGame}
         >
           Start game
